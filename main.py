9e856767edb84ca2e362112031590457d032d521
Afia Caruso <afiacaruso@Baltimores-MacBook-Air.local>
2020-10-14 15:15:51 -0400

    posted data to two endpoints

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data":"Hello World"}
+names = {"afia":{"age":29, "gender":"female"},
+        "casey":{"age":29, "gender":"nonbinary"}}
 
-api.add_resource(HelloWorld,"/helloworld")
+class HelloWorld(Resource):
+    def get(self,name):
+        return names[name]
+    
+    def post(self):
+        return {"data":"Posted"}
+
+api.add_resource(HelloWorld,"/helloworld/<string:name>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-import requests  
+import requests
 
-BASE = "http://127.0.0.1:5000/"
+BASE = "Http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld")
+response = requests.get(BASE + "helloworld/afia/29" )
+
 print(response.json())
